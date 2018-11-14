ac49514fa258e40da703e22d9895e259426044fd
Samuel Prada <jspradac@gmail.com>
2018-11-14 20:01:22 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,9 +1,8 @@
 def reformat_languages(languages)
   hash = {}
   languages.each do |style, lang_hash|
-    a = style 
     lang_hash.each do |language, attributes|
-      b = language
+      
     end
   end 
   hash 
