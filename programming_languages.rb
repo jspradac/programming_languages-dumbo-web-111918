9a46db4914d1ee0b638dfab193c12295c74cdfd5
Samuel Prada <jspradac@gmail.com>
2018-11-14 21:01:52 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -7,10 +7,11 @@ def reformat_languages(languages)
     end
   end 
   languages.each do |styl, lang_hash|
+    a = styl
     lang_hash.each do |language, attributes|
       hash[language] = attributes
-      if hash[language][:style] != [styl]
-        hash[language][:style] << styl
+      if hash[language][:style] != [a]
+        hash[language][:style] << a
       end 
     end
   end 
